e8598ae0058c8649c249f3bea9d368ccbc07c969
Skufu <agpfrancisco1@gmail.com>
2025-03-06 16:11:36 +0800

    repl_test added

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-    fmt.Println("Hello, world!")
+    fmt.Println("Hello, World!")
 }
+
+func cleanInput(text string) []string{
+	lowerText := strings.ToLower(text)
+	trimmedText := strings.TrimSpace(lowerText)
+
+	words := strings.Fields(trimmedText)
+	return words
+}
+
+
+
+
